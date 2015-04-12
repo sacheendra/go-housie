b674f9ecf1799c7b3e947707ab75660ba442596b
Sacheendra Talluri <sacheendra.t@gmail.com>
2015-04-12 16:17:25 +0530

    working program

--- main.go
+++ main.go
@@ -5,8 +5,6 @@ import (
 	"math/rand"
 )
 
-type PersonID int
-
 func contains(s []int, e int) bool {
 	for _, a := range s {
 		if a == e {
@@ -16,15 +14,14 @@ func contains(s []int, e int) bool {
 	return false
 }
 
-func Person(called_number chan int, claim_prize chan PersonID, received chan bool, coupon []int, person_id PersonID) {
+func Person(called_number chan int, claim_prize chan int, received chan bool, coupon []int, person_id int) {
 	numFound := 0
-	for i := 0; i < len(coupon); i++ {
-		current_number := <-called_number
-		found := contains(coupon, current_number)
-		if found {
+	for current_number := range called_number {
+		if contains(coupon, current_number) {
 			numFound++
 		}
 		if numFound == len(coupon) {
+			fmt.Println(person_id)
 			claim_prize <- person_id
 		} else {
 			received <- true
@@ -33,9 +30,15 @@ func Person(called_number chan int, claim_prize chan PersonID, received chan boo
 }
 
 func main() {
-	var called_number chan int
-	var claim_prize chan PersonID
-	var received chan bool
+	var (
+		called_number = make([]chan int, 3)
+		claim_prize   = make(chan int, 1)
+		received      = make(chan bool, 1)
+	)
+
+	called_number[0] = make(chan int, 1)
+	called_number[1] = make(chan int, 1)
+	called_number[2] = make(chan int, 1)
 
 	tokens := make([][]int, 3)
 	for i := 0; i < 3; i++ {
@@ -51,14 +54,16 @@ func main() {
 		}
 	}
 
-	go Person(called_number, claim_prize, received, tokens[0], 0)
-	go Person(called_number, claim_prize, received, tokens[1], 1)
-	go Person(called_number, claim_prize, received, tokens[2], 2)
+	for i := 0; i < 3; i++ {
+		go Person(called_number[i], claim_prize, received, tokens[i], i)
+	}
 
-	claimants := make([]PersonID, 0)
+	claimants := make([]int, 0)
 	prev_called := make(map[int]bool)
 	for i := 0; i < 100; i++ {
 		if len(claimants) == 3 {
+			close(received)
+			close(claim_prize)
 			break
 		}
 		num := rand.Intn(100) + 1
@@ -68,16 +73,29 @@ func main() {
 			_, ok = prev_called[num]
 		}
 		prev_called[num] = true
-		called_number <- num
-		for j := 0; j < 3; j++ {
+
+		for k := 0; k < 3; k++ {
+			if !contains(claimants, k) {
+				called_number[k] <- num
+			}
+		}
+		for j := 0; j < 3-len(claimants); j++ {
 			select {
 			case _ = <-received:
 				continue
 			case pid := <-claim_prize:
+				if pid == 0 {
+					close(called_number[0])
+				}
+				if pid == 1 {
+					close(called_number[1])
+				}
+				if pid == 2 {
+					close(called_number[2])
+				}
 				claimants = append(claimants, pid)
 			}
 		}
 	}
-
 	fmt.Println(claimants)
 }
